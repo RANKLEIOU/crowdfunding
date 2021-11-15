9c4054c3fbd11cc499c85dcaaadba8051773769d
ajv <ajv@rankle.com>
2021-11-16 00:14:16 +0800

    处理返回数据的工具类

---
+++ ajvcrowdfunding-common-util/src/main/java/com/ajv/crowd/util/ResultEntity.java
@@ -0,0 +1,15 @@
+package com.ajv.crowd.util;
+
+import lombok.Data;
+
+@Data
+public class ResultEntity<T> {
+
+	public static final String SUCCESS = "SUCCESS";
+	public static final String FAILED = "FAILED";
+	public static final String NO_MESSAGE = "NO_MESSAGE";
+	public static final String NO_DATA = "NO_DATA";
+
+
+
+}
