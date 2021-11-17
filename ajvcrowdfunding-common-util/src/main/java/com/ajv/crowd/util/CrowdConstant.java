b21ea65e80e04d9c7d14a5ca97bae454f53ff3aa
ajv <ajv@rankle.com>
2021-11-17 17:00:50 +0800

    添加MD5加密算法

--- ajvcrowdfunding-common-util/src/main/java/com/ajv/crowd/util/CrowdConstant.java
+++ ajvcrowdfunding-common-util/src/main/java/com/ajv/crowd/util/CrowdConstant.java
@@ -2,8 +2,9 @@ package com.ajv.crowd.util;
 
 public class CrowdConstant {
 
-	public static final String MESSAGE_LOGIN_FAILED = "账号密码错误，请重新输入";
 	public static final String MESSAGE_ACCESS_FORBIDEN = "请登录以后再访问";
+	public static final String MESSAGE_LOGIN_FAILED = "账号密码错误，请重新输入";
+	public static final String MESSAGE_STRING_INVALIDATE = "字符串不能为空！请输入有效字符";
 
 	public static final String ATTR_NAME_EXCEPTION = "exception";
 }
--- ajvcrowdfunding-common-util/src/main/java/com/ajv/crowd/util/CrowdUtil.java
+++ ajvcrowdfunding-common-util/src/main/java/com/ajv/crowd/util/CrowdUtil.java
@@ -1,12 +1,55 @@
 package com.ajv.crowd.util;
 
 import javax.servlet.http.HttpServletRequest;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class CrowdUtil {
+	/**
+	 * 对明文密码进行加密处理
+	 * @param source 传入需要加密的明文
+	 * @return 加密结果
+	 */
+	public static String md5(String source){
+
+		// 判断字符是否有效
+		if (source == null || source.length() == 0){
+			//如果无效则抛出异常
+			throw new RuntimeException(CrowdConstant.MESSAGE_STRING_INVALIDATE);
+		}
+
+		try {
+			// 获取MessageDigest对象
+			String algorithm = "md5";
+			MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+
+			// 获取明文字符串的字节数组
+			byte[] input = source.getBytes();
+
+			// 执行加密
+			byte[] output = messageDigest.digest(input);
+
+			// 创建bigInteger对象
+			// 规定返回的值-1为负数，0为0，1为正数
+			int signum = 1;
+			BigInteger bigInteger = new BigInteger(signum,output);
+
+			//按照16进制将bigInteger的值转换为字符串
+			int radix = 16;
+			String encoded = bigInteger.toString(radix).toUpperCase();
+
+			return encoded;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	/**
 	 * 判断是否为Ajax请求
-	 * @param request
+	 * @param request 用户发送的请求
 	 * @return true表示该请求是Ajax请求
 	 * 			false表示该请求是普通请求
 	 */
