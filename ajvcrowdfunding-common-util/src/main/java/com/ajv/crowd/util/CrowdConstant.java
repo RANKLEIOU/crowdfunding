f44968356c6c061a8bc3e366f3aa92d1b56ad2a6
ajv <ajv@rankle.com>
2021-11-17 12:08:41 +0800

    常量管理类，减少错误

---
+++ ajvcrowdfunding-common-util/src/main/java/com/ajv/crowd/util/CrowdConstant.java
@@ -0,0 +1,9 @@
+package com.ajv.crowd.util;
+
+public class CrowdConstant {
+
+	public static final String MESSAGE_LOGIN_FAILED = "账号密码错误，请重新输入";
+	public static final String MESSAGE_ACCESS_FORBIDEN = "请登录以后再访问";
+
+	public static final String ATTR_NAME_EXCEPTION = "exception";
+}
