e52cb6dca1a83a341a5e25401a340e7ab25066d3
ajv <ajv@rankle.com>
2021-11-17 19:17:10 +0800

    添加登录相关常量

--- ajvcrowdfunding-common-util/src/main/java/com/ajv/crowd/constant/CrowdConstant.java
+++ ajvcrowdfunding-common-util/src/main/java/com/ajv/crowd/constant/CrowdConstant.java
@@ -5,6 +5,8 @@ public class CrowdConstant {
 	public static final String MESSAGE_ACCESS_FORBIDEN = "请登录以后再访问";
 	public static final String MESSAGE_LOGIN_FAILED = "账号密码错误，请重新输入";
 	public static final String MESSAGE_STRING_INVALIDATE = "字符串不能为空！请输入有效字符";
+	public static final String MESSAGE_SYSTEM_ERROR_LOGIN_NOT_UNIQUE = "系统错误！该账号不唯一！";
 
 	public static final String ATTR_NAME_EXCEPTION = "exception";
+	public static final String ATTR_NAME_LOGIN_ADMIN = "loginAdmin";
 }
