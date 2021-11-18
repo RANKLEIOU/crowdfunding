3d6a1aa7074e43bd73b4121cd7e3638ef2aba9b4
ajv <ajv@rankle.com>
2021-11-18 09:07:56 +0800

    完成登录跳转和退出登录

--- ajvcrowdfunding-admin-parent/ajvcrowdfunding-admin-component/src/main/java/com/ajv/crowd/mvc/controller/AdminController.java
+++ ajvcrowdfunding-admin-parent/ajvcrowdfunding-admin-component/src/main/java/com/ajv/crowd/mvc/controller/AdminController.java
@@ -16,10 +16,29 @@ public class AdminController {
 	@Autowired
 	private AdminService adminService;
 
+	/**
+	 * 登录方法
+	 * @param loginAcct 从前端传入的用户账号
+	 * @param userPswd 从前端传入的用户密码
+	 * @param session 用于将登录的用户信息存入session域中
+	 * @return 登录成功后重定向到管理员后台界面
+	 */
 	@RequestMapping("/admin/do/login")
 	public String doLogin(@RequestParam("loginAcct")String loginAcct, @RequestParam("userPswd")String userPswd, HttpSession session){
 		Admin admin = adminService.getAdminByLoginAcct(loginAcct,userPswd);
 		session.setAttribute(CrowdConstant.ATTR_NAME_LOGIN_ADMIN,admin);
-		return "admin-main";
+		return "redirect:/admin/to/main/toMain";
+	}
+
+	/**
+	 * 退出登录
+	 * @param session 让登录后存入的用户信息失效
+	 * @return 退出登录后跳转到登陆页面
+	 */
+	@RequestMapping("/admin/do/logout")
+	public String doLogout(HttpSession session){
+		// 强制session域对象失效
+		session.invalidate();
+		return "redirect:/admin/to/login/toLogin";
 	}
 }
