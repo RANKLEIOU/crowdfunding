e09462045ee61aad2e255486f9990020645f81a3
ajv <ajv@rankle.com>
2021-12-13 19:35:54 +0800

    生成角色相关数据

---
+++ ajvcrowdfunding-admin-parent/ajvcrowdfunding-admin-entity/src/main/java/com/ajv/crowd/entity/Role.java
@@ -0,0 +1,15 @@
+package com.ajv.crowd.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+	private Integer id;
+
+	private String roleName;
+
+}
