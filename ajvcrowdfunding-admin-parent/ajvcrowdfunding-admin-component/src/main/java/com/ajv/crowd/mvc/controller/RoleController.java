99d054bf1ad5f6ca638be189a8a350a19fe5335b
ajv <ajv@rankle.com>
2021-12-14 09:20:35 +0800

    编写角色控制层分页查询功能

---
+++ ajvcrowdfunding-admin-parent/ajvcrowdfunding-admin-component/src/main/java/com/ajv/crowd/mvc/controller/RoleController.java
@@ -0,0 +1,32 @@
+package com.ajv.crowd.mvc.controller;
+
+import com.ajv.crowd.constant.CrowdConstant;
+import com.ajv.crowd.entity.Role;
+import com.ajv.crowd.service.api.RoleService;
+import com.ajv.crowd.util.ResultEntity;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RoleController {
+
+	@Autowired
+	private RoleService roleService;
+
+	@RequestMapping("/role/get/page")
+	@ResponseBody
+	public ResultEntity<PageInfo> rolePageInfo(@RequestParam(value = "keyword", defaultValue = "") String keyword,
+									 @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+									 @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+									 Model model) {
+
+		PageInfo<Role> pageInfo = roleService.getRoleByKeyword(keyword, pageNum, pageSize);
+		model.addAttribute(CrowdConstant.ATTR_NAME_PAGE_INFO,pageInfo);
+		return ResultEntity.successWithData(pageInfo);
+	}
+}
